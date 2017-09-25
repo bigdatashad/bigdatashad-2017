df96a71329f2d0263e654eb3c63c97fe4b6bc316
Anton Gorokhov <anton.gorokhov@gmail.com>
2017-09-25 16:55:23 +0300

    hw1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Методы и системы обработки больших данных
+
+В данном репозитории публикуются материалы по курсу "Методы и системы обработки больших данных",
+читаемому в ШАД Яндекса осенью 2017 года.
+
+[https://wiki.school.yandex.ru/shad/groups/2016/Semester3/BigData1/](https://wiki.school.yandex.ru/shad/groups/2016/Semester3/BigData1/)
+
---
+++ assignments/hw1/example.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import getpass
+import hashlib
+import random
+import struct
+
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+app.secret_key = "my_secret_key"
+
+
+def iterate_between_dates(start_date, end_date):
+    span = end_date - start_date
+    for i in xrange(span.days + 1):
+        yield start_date + datetime.timedelta(days=i)
+
+
+@app.route("/")
+def index():
+    return "OK!"
+
+
+@app.route("/api/hw1")
+def api_hw1():
+    start_date = request.args.get("start_date", None)
+    end_date = request.args.get("end_date", None)
+    if start_date is None or end_date is None:
+        abort(400)
+    start_date = datetime.datetime(*map(int, start_date.split("-")))
+    end_date = datetime.datetime(*map(int, end_date.split("-")))
+
+    result = {}
+    for date in iterate_between_dates(start_date, end_date):
+        total_hits = int(random.normalvariate(1000, 50))
+        total_users = int(random.normalvariate(100, 5))
+        result[date.strftime("%Y-%m-%d")] = {
+            "total_hits": total_hits,
+            "total_users": total_users,
+        }
+
+    return jsonify(result)
+
+
+def login_to_port(login):
+    """
+    We believe this method works as a perfect hash function
+    for all course participants. :)
+    """
+    hasher = hashlib.new("sha1")
+    hasher.update(login)
+    values = struct.unpack("IIIII", hasher.digest())
+    folder = lambda a, x: a ^ x + 0x9e3779b9 + (a << 6) + (a >> 2)
+    return 10000 + reduce(folder, values) % 20000
+
+
+def main():
+    parser = argparse.ArgumentParser(description="HW 1 Example")
+    parser.add_argument("--host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=int, default=login_to_port(getpass.getuser()))
+    parser.add_argument("--debug", action="store_true", dest="debug")
+    parser.add_argument("--no-debug", action="store_false", dest="debug")
+    parser.set_defaults(debug=False)
+
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=args.debug)
+
+if __name__ == "__main__":
+    main()
